5ea1edbfbd8f24b22523fb77b8b729927895b7f8
Ben Noyes <noyesbenjamin@gmail.com>
2026-03-27 13:35:59 -0400

    wrong parameter store param name

--- utils/secrets.py
+++ utils/secrets.py
@@ -4,4 +4,4 @@ from components.read_secrets import get_parameter
 REGION = "us-west-1"
 
 aws_region = get_parameter(REGION, "/we_better/aws_region")
-aws_bucket_name = get_parameter(REGION, "/we_better/aws_bucket_name")
+aws_bucket_name = get_parameter(REGION, "/we_better/s3_bucket_name")
