13a8be61301f4e48a03eb82a4d4ad6188c38c517
Ben Noyes <noyesbenjamin@gmail.com>
2026-03-28 06:32:52 -0400

    updated card copy

--- components/cards.py
+++ components/cards.py
@@ -8,7 +8,7 @@ top_cards = dmc.Group(
         card(84, "countries", colors["blue"]),
         card(11, "initial predictors", colors["blue"]),
         card(4, "final predictors", colors["blue"]),
-        card(0.231, "r2", colors["blue"]),
+        card(0.231, "final model r2", colors["blue"]),
     ],
     className="intro-lyt",
     gap="xs",
