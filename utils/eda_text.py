4188ee550a2235e98ab65f04a56ada2af1e5cd5a
Ben Noyes <noyesbenjamin@gmail.com>
2026-03-28 07:24:26 -0400

    updated eda summary copy

--- utils/eda_text.py
+++ utils/eda_text.py
@@ -1,4 +1,4 @@
 eda_text_dct = {
     0: "Explore the data visually in one, two, or three dimensions.",
-    1: "In two and three dimensions, the model fit is a natural cubic spline with the natural spline constraint included (linear at the boundaries). Due to the small sample size, which increases the risk of overfitting without sufficient smoothing, a large lambda was used; k knots were set automatically through cross validation with pyGAM.",
+    1: "In two and three dimensions, the model fit is a natural cubic spline with the natural spline constraint included (linear at the boundaries). Due to the small sample size, the spline model is at risk of overfitting the data; however, higher order polynomial functions generally did not perform well in EDA (degrees1-5 were iterated over, inclusive), and a smoothing spline captures the uptick in the floor effect quite well. While multi linear regression was used for the final model for generalizability, splines provide a nice peek into the nuance of the relationship of this specific sample. Lambda and K knots were set automatically through cross validation with pyGAM.",
 }
